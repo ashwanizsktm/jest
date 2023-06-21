d7a09765ad6d4870d495667fc7f6f34869fdda9e
Ashwani <ashwani.kumar@goldenpi.com>
2023-06-21 09:36:09 +0530

    initial commit

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,11 @@
+
+// import { AppComponent } from './app.component';
+describe("app component", ()=> {
+    it('title shold be Angular', ()=> {
+        // let component: AppComponent;
+        // let title = component.title;
+        let x = 2;
+        expect(x).toBe(2);
+        
+    })
+})
